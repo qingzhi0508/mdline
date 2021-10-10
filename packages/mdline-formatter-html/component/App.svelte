2c42075e592c9dc2757fe37db1ecec49610e9caa
cayden <qingzhi_xie@foxmail.com>
2021-10-10 21:57:59 +0800

    custom-mdline

--- packages/mdline-formatter-html/component/App.svelte
+++ packages/mdline-formatter-html/component/App.svelte
@@ -1,125 +1,124 @@
+
 <section class="box">
-  <ul id="first-list">
-    {#each items as item,index}
-  
-    <li>
-        <span></span>
-        <h1 class="title">{item.title}</h1>
-        <div class="info">{@html item.bodyHTML}</div>
-        <div class="name"></div>
-        <div class="time">
-            <span>{item.beginDate}</span>
+
+    {#each items.reverse() as item,index}
+    
+    <div class="listItem" data-index="{index}">
+    
+     <div class="listItemContent">
+        <div class="listItemContent-date"> <span>{item.beginDate}</span>
             <span>{#if item.endDate}{item.endDate}{/if}</span>
         </div>
-    </li>
+        <h3  class="title">{item.title}</h3>
+        <div class="listItemContent-content">{@html item.bodyHTML}</div>
+     </div>
+    </div>
     {/each}
+    </section>
     
-  </ul>
     
-   
-  
-  </section>>
-  
-  <style>
-  body{
-      margin: 1;
-      padding: 1;
-      font-family: arial
-  }
-  
-  .box{
-      overflow: hidden;
-      padding: 10px 0 40px 60px
-  }
-  
-  .box ul{
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      position: relative;
-      transition: all 0.5s linear;
-      top:0
-  }
-  
-  .box ul:last-of-type{top:80px}
-  
-  .box ul:before {
-      content: "";
-      display: block;
-      width: 0;
-      height: 100%;
-      border: 1px dashed #968b8b;
-      position: absolute;
-      top: 0;
-      left:  -6.3%;
-  }
-  
-  .box ul li{
-      margin: 0 0 8%;
-      position: relative;
-      padding: 2% 1%;
-      border-radius: 10px;
-      border: 1px solid rgb(222, 222, 222);
-     
-      margin-left: 5%;
-  }
-  
-  
-  .box ul li > span{
-      content: "";
-      display: block;
-      width: 0;
-      height: 100%;
-      border:1px solid #afa8a8;
-      position: absolute;
-      top:0;
-      left:-12%;
-  }
-  
-  .box ul li > span:before,.box ul li > span:after{
-      content: "";
-      display: block;
-      width: 20px;
-      height: 20px;
-      border-radius: 50%;
-      position: absolute;
-  }
-  
-  .box ul li > span:before{top:-10px}
-  .box ul li > span:after{top:95%}
-  
-  .box .title{
-      color: #5ed3bf;
-      font-size: 1.4em;
-      margin: 0;
-      padding: 0 0 0.5em 0;
-      letter-spacing: 0.1em;
-  }
-  
-  .box .info:first-letter{text-transform: capitalize;line-height: 1.7}
-  
-  .box .name{
-      margin-top: 10px;
-      text-transform: capitalize;
-      font-style: italic;
-      text-align: right;
-      margin-right: 20px
-  }
-  
-  .box .time span{
-    position: absolute;
-      left: -14%;
-      font-size: 80%;
-      font-weight: bold;
-      display: block;
-      clear: both;
-      padding: 0.25em 0;
-      border: 1px solid rgb(222, 222, 222);
-      background-color: #80e6baba;
-  }
-  .box .time span:first-child{top:-16px}
-  .box .time span:last-child{top:94%}
-  
-    </style>
-  
-  
+    
+    <style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        font-size: 14px;
+        -webkit-user-select: none;
+        -webkit-touch-callout: none;
+        -webkit-tap-highlight-color:rgba(0, 0, 0, 0);
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-size-adjust: 100%;
+        font-family:"Helvetica Neue", Helvetica, Arial, sans-serif;
+        word-wrap: break-word;
+    
+        padding-bottom: 20px;
+    }
+    img {
+        width: 100%;
+        display: block;
+    }
+    
+    
+    #list {
+        margin: 0 4px 0;
+        overflow-x: hidden;
+        overflow-y: auto;
+    }
+    
+    
+    .title {
+        color: #5ed3bf;
+        font-size: 1.4em;
+    }
+    .listItem {
+        position: relative;
+        padding-left: 40px;
+        padding-top: 4px;
+    }
+    .listItem:before {
+        content: "";
+        display: inline-block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 1px;
+        height: 100%;
+        border-right: 1px solid rgb(168, 163, 163);
+        left: 19px;
+        z-index: 1;
+    }
+    .listItem-first:before {
+        height: 50%;
+        top: 50%;
+    }
+    .listItem-last:before {
+        height: 50%;
+    }
+    .listItem:after {
+        content: "";
+        display: inline-block;
+        position: absolute;
+        width: 8px;
+        height: 8px;
+        background-color: #5ed3bf;
+        border-radius: 4px;
+        left: 16px;
+        top: 50%;
+        margin-top: -2px;
+        z-index: 1;
+    }
+    .listItem.highlight:after {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        width: 16px;
+        height: 16px;
+        background-color: #3d93fd;
+        border: 4px solid #88bdfe;
+        border-radius: 8px;
+        left: 12px;
+        -webkit-box-shadow: 0 0 0 3px #d8e9ff;
+        box-shadow: 0 0 0 3px #d8e9ff;
+        z-index: 2;
+    }
+    .listItem.highlight > .listItemContent {
+        background-color: #3d93fd;
+        color: #fff;
+    }
+    .listItem.highlight .listItemContent-date{
+        color: rgb(24, 22, 22);
+    }
+    
+    .listItemContent {
+        padding: 5px;
+        border: 1px solid rgb(172, 169, 169);
+        border-radius: 4px;
+    }
+    .listItemContent-date {
+        color: #161515;
+    }
+      </style>
+    
+    
